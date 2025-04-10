b5aed233894f2d5326308a52ee5a924528b25a40
mauboru <josue21henrique@gmail.com>
2025-04-10 18:11:08 -0300

    isnerindo novo rota de api

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-// const API_BASE_URL = "http://85.31.63.241:3001";
-const API_BASE_URL = "http://172.29.160.1:3001";
+const API_BASE_URL = "http://212.85.19.3:3001";
+// const API_BASE_URL = "http://172.29.160.1:3001";
 
 export default API_BASE_URL;
